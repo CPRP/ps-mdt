79fef60a56600b41ba0fbde0380b761158e4cfed
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-09-08 16:02:58 -0400

    Added San Andreas Police Force

--- ui/app.js
+++ ui/app.js
@@ -3551,7 +3551,7 @@ $(document).ready(() => {
           "#8f741b"
         );
         $(".badge-logo").attr("src", "img/sasp_badge.png");
-        $(".header-title").html("SAN ANDREAS STATE POLICE");
+        $(".header-title").html("SAN ANDREAS POLICE FORCE");
         $(".bolo-nav-item").html("BOLOs");
         $(".bolos-search-title").html("Bolos");
         $("#bolos-search-input").attr("placeholder", "Search Bolo...");
